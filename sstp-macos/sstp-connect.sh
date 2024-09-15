648ed6e45d8b113bf64bed2485a9cea3d5a4987d
Aman <42399567+amanx100@users.noreply.github.com>
2024-09-15 12:09:50 +0600

    updated to handle errors

--- sstp-macos/sstp-connect.sh
+++ sstp-macos/sstp-connect.sh
@@ -49,7 +49,8 @@ CERT_FILE="/tmp/sstpc_certificate.crt"
 # Function to create the certificate file
 create_certificate() {
     echo "$CERTIFICATE_CONTENT" > "$CERT_FILE"
-    echo "Certificate created at: $CERT_FILE"
+    #echo "Certificate created at: $CERT_FILE"
+    echo "Certificate created"
 }
 
 # Function to clean up the PID and certificate files
@@ -76,20 +77,53 @@ cleanup
 create_certificate
 
 # Start a new SSTP connection in the background
+# Add this two params if need to add gateway: usepeerdns defaultroute
 sudo sstpc --ca-cert "$CERT_FILE" --user $VPN_USER --password $VPN_PASS $VPN_HOSTP require-mschap-v2 noauth &
 
 # Capture the new process ID of the SSTP connection
 VPN_PID=$!
-echo "New VPN process started with PID: $VPN_PID"
+echo "Attempting to start VPN connection with PID: $VPN_PID"
 
-# Save the new PID to the PID file
-echo $VPN_PID > "$PID_FILE"
-
-# Sleep for a few seconds to ensure SSTP connection is established
+# Wait for a few seconds to ensure SSTP connection is established
 sleep 5
 
-# Add the specific route
-sudo route add -net $VPN_ROUTE -interface ppp0
+# Check if SSTP process is still running
+if ps -p $VPN_PID > /dev/null; then
+    # SSTP connection is successful
+    echo "VPN connection established successfully with PID: $VPN_PID"
+    
+    # Save the new PID to the PID file
+    echo $VPN_PID > "$PID_FILE"
+    
+    if [ -n "$VPN_ROUTE" ]; then
+        # Add the specific route
+        sudo route add -net $VPN_ROUTE -interface ppp0
+        
+        # Print a success message
+        echo "Route added successfully to network $VPN_ROUTE via interface ppp0."
+    else
+        echo "VPN custom route is not assigned"
+    fi
 
-# Print a success message
-echo "VPN connected and route added. New SSTP PID: $VPN_PID"
+    if [ -n "$VPN_ROUTE2" ]; then
+        # Add the specific route
+        sudo route add -net $VPN_ROUTE2 -interface ppp0
+        
+        # Print a success message
+        echo "Route 2 added successfully to network $VPN_ROUTE2 via interface ppp0."
+    else
+        echo "VPN custom route 2 is not assigned"
+    fi
+
+    
+else
+    # SSTP connection failed
+    echo "Error: Failed to establish VPN connection."
+    
+    # Clean up if necessary
+    cleanup
+
+    # Exit the script with an error status
+    exit 1
+fi
+
